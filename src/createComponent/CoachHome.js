1a0ee0da6f9ba78bff49f3995cd861e4c710bc8d
Shivam Barthwal <shivambarthwal@live.com>
2019-11-19 16:37:40 +0100

    Shivam ~ Coach Notifications set up in Frontend done

--- src/createComponent/CoachHome.js
+++ src/createComponent/CoachHome.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { withStyles, Snackbar, SnackbarContent, IconButton } from '@material-ui/core';
+import clsx from 'clsx';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import PropTypes from 'prop-types';
+// import Button from '@material-ui/core/Button';
+import { SnackbarProvider, withSnackbar } from 'notistack';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import { amber, green } from '@material-ui/core/colors';
+import WarningIcon from '@material-ui/icons/Warning';
+
 import '../assets2/web/assets/mobirise-icons/mobirise-icons.css';
 import '../assets2/bootstrap/css/bootstrap.min.css';
 import '../assets2/bootstrap/css/bootstrap-grid.min.css';
@@ -9,11 +23,98 @@ import '../assets2/theme/css/style.css';
 import '../assets2/mobirise/css/mbr-additional.css';
 import '../assets2/mobirise/css/mbr-additional.css';
 var Logo = require('../assets2/images/logo-mzt.png');
+const customer_Id = "5dc53fb7717676384459fe63";
+const variantIcon = {
+    success: CheckCircleIcon,
+    warning: WarningIcon,
+    error: ErrorIcon,
+    info: InfoIcon,
+};
 
-export default class CoachHome extends Component {
+const styles = theme => ({
+    success: {
+      backgroundColor: green[600],
+    },
+    error: {
+      backgroundColor: theme.palette.error.dark,
+    },
+    info: {
+      backgroundColor: theme.palette.primary.main,
+    },
+    warning: {
+      backgroundColor: amber[700],
+    },
+    icon: {
+      fontSize: 20,
+    },
+    iconVariant: {
+      opacity: 0.9,
+      marginRight: theme.spacing(1),
+    },
+    msg: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+ 
+});
+
+class CoachHome extends Component {
+    state = {
+        notifications: [
+            // {
+            //     customer_id: "5dc53fb7717676384459fe63",
+            //     coach_id: "5dc2f70414b9e52a30d6620e",
+            //     notify_for: "COACH",
+            //     notify_type: "Program Assigned",
+            //     message: `A new program (Muscle Gain) has been assigned to you`
+            // },
+            // {
+            //     customer_id: "5dc53fb7717676384459fe63",
+            //     coach_id: "5dc2f70414b9e52a30d6620e",
+            //     notify_for: "COACH",
+            //     notify_type: "Program Assigned",
+            //     message: `A new focus session has been validated`
+            // }
+        ],
+        open: false,
+        vertical: 'top',
+        horizontal: 'right'
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:8080/notification/checkAllNotification`,
+        {
+            params: {
+                notify_for: "COACH",
+                customer_id: customer_Id,
+                coach_id: "5dc2f70414b9e52a30d6620e"
+            }
+        })
+        .then(res => {
+            const notifications = res.data;
+            this.setState({notifications});
+        });
+    };
+
+    
+    handleClickNotif = () => {
+        const { notifications, vertical, horizontal } = this.state;
+        notifications.forEach(notification =>
+            setTimeout(() => this.props.enqueueSnackbar(notification.msg, {variant: 'info'}), 200));
+            
+    };
+
+    handleClose = () => {
+        this.setState({ open: false});
+    };
     render() {
+        const { notifications } = this.state;
+        const { classes, className } = this.props;
+        const Icon = variantIcon["warning"];
         return (
-            <body>
+            <SnackbarProvider>
+                <div className="App">  
+                <body>
                 <section class="menu cid-rFxS6PmLUN" id="menu1-r">
                     <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
                         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -49,6 +150,7 @@ export default class CoachHome extends Component {
                             <a class="btn btn-white-outline" href="visualizeSession">SESSIONS </a>
                             <a class="btn btn-white-outline" href="visualizeProgram">PROGRAMS </a> 
                             <a class="btn btn-white-outline" href="customerList">CUSTOMERS</a>
+                            <a class="btn btn-white-outline" onClick={this.handleClickNotif}>NOTIFICATIONS ({notifications.length})</a>
                         </div>
                     </div>    
                 </section>
@@ -62,6 +164,10 @@ export default class CoachHome extends Component {
                 <script src="assets2/touchswipe/jquery.touch-swipe.min.js"></script>
                 <script src="assets2/theme/js/script.js"></script>
             </body>
-        )
-    }
-}
+            </div>
+            </SnackbarProvider>           
+        );
+    };
+};
+
+export default withSnackbar(withStyles(styles,{withTheme: true})(CoachHome));
